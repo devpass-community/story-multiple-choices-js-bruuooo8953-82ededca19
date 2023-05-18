7e0bcc5f0eb0a67908ee60b96dd9cafee8aba27b
Bruno Luis <bruno.dev07@gmail.com>
2023-05-18 08:59:50 -0300

    My first commit

--- chooseProduct.js
+++ chooseProduct.js
@@ -1,5 +1,15 @@
 function chooseProduct(segment) {
-    // Add your solution here!
+    switch(segment){
+        case 'Beverage':
+            return 'Soda'
+        break;
+        case 'Fruit':
+            return 'Apple'
+        break;
+        case 'Hygiene':
+            return 'Soap'
+        break;
+    }
 }
 
 module.exports = chooseProduct;
